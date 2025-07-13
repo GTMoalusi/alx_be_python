198787a63ff1daa8973f6082dae4b47e641b1c71
Thabang Moalusi <thabang.moalusi@yahoo.com>
2025-07-13 19:31:04 +0200

    initial commit: Personal Finance Calculator

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,5 +1,5 @@
 #User inputs
-monthly_income = int(input(""Enter your monthly income: ))
+monthly_income = int(input("Enter your monthly income: "))
 monthly_expenses = int(input("Enter your total monthly expenses: "))
 
 #Calculate monthly savings
