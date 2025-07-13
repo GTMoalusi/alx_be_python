3620b55eef3a176077df7d0ca33e8194d6930a82
Thabang Moalusi <thabang.moalusi@yahoo.com>
2025-07-13 19:00:53 +0200

    User Input Age Calculator

---
+++ python_introduction/future_age_calculator.py
@@ -0,0 +1,9 @@
+input_age = int(input("How old are you: "))
+
+current_year = 2023
+
+future_year = 2050
+
+age = input_age + (2050 -2023)
+
+print(f"in {future_year}, you will be {age} years old.")
