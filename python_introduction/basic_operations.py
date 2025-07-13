d03cc494606c705a794dc965b34eec2eb50c2208
Thabang Moalusi <97936411+GTMoalusi@users.noreply.github.com>
2025-07-13 18:12:56 +0200

    Update basic_operations.py

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,13 +1,13 @@
-//Assigning variables
+#Assigning variables
 number1 = 10
 number2 = 5
 
-//basics math operations
+#basics math operations
 sum = number1 + number2
 difference = number1 - number2
 product = number1 * number2
 
-//output or results
+#output or results
 print(f"Addition of {number1} and {number2} is {sum}")
 print(f"Subtraction of {number1} and {number2} is {difference}")
 print(f"Multiplication of {number1} and {number2} is {product}")
