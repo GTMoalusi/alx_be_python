dcbae40ca9f75f432881de41c500d8fd1332f511
Thabang Moalusi <thabang.moalusi@yahoo.com>
2025-07-20 22:50:50 +0200

    Personal Daily Reminder

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -1,6 +1,6 @@
 task = input("Enter your task: ").lower()
 priority = input("Priority (high/medium/low): ").lower()
-time_bound = input("Is it time-bound? (yes/no)").lower()
+time_bound = input("Is it time-bound? (yes/no): ").lower()
 
 match priority:
    case a if priority == "high":
